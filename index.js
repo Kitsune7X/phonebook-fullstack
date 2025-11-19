c3262aee1f92956d05eecdfc4d303de16c34cc00
Kitsune7X <ryuizanagi@proton.me>
2025-11-20 08:15:39 +0900

    .

--- index.js
+++ index.js
@@ -120,8 +120,10 @@ const errorHandler = (err, req, res, next) => {
 
   if (err.name === 'CastError')
     return res.status(400).send({ error: 'malformed id' });
-  else if (err.name === 'ValidationError')
+  else if (err.name === 'ValidationError') {
+    console.log(err);
     return res.status(400).json({ error: err.message });
+  }
 
   next(err);
 };
